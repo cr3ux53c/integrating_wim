0646253aef0b1224abe0f4937cd82873eb062ac2
Hyunseok Lim <cr3ux53c@gmail.com>
2022-05-04 23:26:15 +0900

    Implement export and split

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+import os, pycdlib, configparser
 from colorama import Fore, Style
-import os, pycdlib, ast
+"""
+Fore: BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE, RESET.
+Back: BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE, RESET.
+Style: DIM, NORMAL, BRIGHT, RESET_ALL
+"""
 from pycdlib import pycdlibexception
-import configparser
 from tqdm import tqdm
 from glob import iglob
 from os.path import join, basename, splitext, exists
@@ -10,7 +14,6 @@ from wim import WIM
 CFG_PATH = './wim_exporter.cfg'
 
 def print_wiminfo(wim_path, idx_range: range=None, category: list=None):
-
     if idx_range is None:
         idx_range = range(1, get_max_idx(wim_path)+1, 1)
     
@@ -65,64 +68,32 @@ def get_max_idx(wim_path):
         if 'Index' in lines[idx]:
             return int(lines[idx].split(':')[1])
 
-def desc(src, idx=None, category: list=None):
-    cmd = 'dism /get-wiminfo /wimfile:{}{} /english'.format(src, ' /index:{}'.format(idx) if idx is not None else '')
-    if category is not None:
-        if idx is not None:
-            cnsl_redir = os.popen(cmd)
-            for li in cnsl_redir.readlines():
-                if li.split(':')[0] in ' '.join(category):
-                    print(li, end='')
+def sort_wim(x):
+    from packaging.version import parse
+    packed_return = []
+
+    for k, v in sort_criteria.items():
+        if len(v):
+            packed_return.append(sort_criteria[k].index(eval('x.' + k)))
         else:
-            for idx in range(1, get_max_idx(src)+1):
-                cmd = 'dism /get-wiminfo /wimfile:{} /index:{} /english'.format(src, idx)
-                detail = os.popen(cmd).readlines()
-                for det_idx in range(len(detail)):
-                    cri = detail[det_idx].replace('\n', '').replace('\t', '')
-                    if len(cri) > 0 and cri.split(':')[0].replace(' ', '') in ' '.join(category):
-                        if cri.split(':')[0].replace(' ', '') == 'Languages':
-                            print(cri + ' ' + detail[det_idx+1].replace('\t', ''))
-                        else:
-                            print(cri)
-    else:
-        return os.system(cmd)
+            packed_return.append((parse(eval('x.' + k))))
 
-def export(src, dst, idx=None):
-    if idx is not None:
-        os.system('dism /export-image /sourceimagefile:{} /sourceindex:{} /destinationimagefile:{} /english'.format(src, idx, dst))
-    else:
-        for idx in range(1, get_max_idx(src)+1):
-            os.system('dism /export-image /sourceimagefile:{} /sourceindex:{} /destinationimagefile:{} /english'.format(src, idx, dst))
+    return packed_return[0] if len(packed_return) == 1 else packed_return[0], *packed_return[1:]
 
+def export(src_path, dst_path, idx_range: range=None, compress: str=None, check_integrity=False):
+    if idx_range is None: 
+        idx_range = range(1, get_max_idx(src_path)+1)
 
-# def sort(src, dst, criteria):
-#     criteria.reverse()
-#     max_idx = get_max_idx(src)
-#     init_sorted = [i for i in range(1, max_idx+1)]
-    
-#     details = []
-#     for i in init_sorted:
-#         cmd = 'dism /get-wiminfo /wimfile:{} /index:{} /english'.format(src, i)
-#         details.append(os.popen(cmd).readlines())
-    
-#     ret = srt(details, criteria)
+    for idx in idx_range:
+        """
+        /Export-Image Usage
 
-# def srt(details, criteria):
-#     if len(criteria) > 1:
-#         srt(details, criteria[1:])
-#     criterion = criteria[0]
-    
-#     sorted_details = []
-#     for detail in details:
-#         for det in detail:
-#             if criterion == det.split('')[0]:
-#                 value = det.split('')[1].replace('\n', '').replace('\t', '')
-#                 sorted_details.append(detail)
-#                 break
-#     return sorted_details
+        Dism /Export-Image /SourceImageFile:<path_to_image_file> {/SourceIndex:<image_index> | /SourceName:<image_name>} /DestinationImageFile:<path_to_image_file> [/DestinationName:<Name>] [/Compress:{fast|max|none|recovery}] [/Bootable] [/WIMBoot] [/CheckIntegrity]
+        """
+        os.system(f'dism /export-image /sourceimagefile:{src_path} /sourceindex:{idx} /destinationimagefile:{dst_path}{" /compress:" + compress if compress else ""}{" /checkintegrity" if check_integrity else ""} /english')
 
-def wim2swm(src, dst, size=4095, chk_integrity=False):
-    os.system('dism /split-image /imagefile:{} /swmfile:{} /filesize:{}{}'.format(src, dst, size, ' /checkintegrity' if chk_integrity else ''))
+def wim2swm(src_path, dst_path, size=4095, check_integrity=False):
+    os.system(f'dism /split-image /imagefile:{src_path} /swmfile:{dst_path} /filesize:{size}{" /checkintegrity" if check_integrity else ""}')
 
 def create_cfg(config):
     config['DEFAULT'] = {
@@ -147,19 +118,6 @@ def create_cfg(config):
     
     return config
 
-from packaging.version import parse
-def sort_wim(x):
-    from packaging.version import parse
-    global sort_count, sort_keys
-    packed_return = []
-
-    for k, v in sort_criteria.items():
-        if len(v):
-            packed_return.append(sort_criteria[k].index(eval('x.' + k)))
-        else:
-            packed_return.append((parse(eval('x.' + k))))
-
-    return packed_return[0] if len(packed_return) == 1 else packed_return[0], *packed_return[1:]
 
 if __name__ == '__main__':
 
@@ -180,6 +138,12 @@ if __name__ == '__main__':
     src_dir = config['DEFAULT']['src_dir']
     dst_dir = config['DEFAULT']['dest_dir']
     sort_criteria = eval(config['Export']['sort_criteria'])
+    enable_pause = int(config['DEFAULT']['enable_pause'])
+    split_wim = int(config['Features']['split_wim'])
+    purge_old_wim = int(config['Export']['purge_old_wim'])
+    dest_wim_fname = config['Export']['dest_wim_fname']
+    dest_swm_fname = config['Export']['dest_swm_fname']
+    split_size = int(config['Export']['split_size'])
     sort_count = -1
 
     '''
@@ -205,132 +169,36 @@ if __name__ == '__main__':
                 tqdm.write(str(e) + f': {iso_path}')
 
     '''
-    Summary wim-files.
+    Export wim-files to single wim file.
     '''
 
-    if int(config['Features']['get_wiminfo']):
+    if int(config['Features']['export_wim']):
         wim_paths = list(iglob(join(dst_dir, '*.' + target_ext)))
         wim_indexes = []
         for wim_path in tqdm(wim_paths, 'Opening WIM file', dynamic_ncols=True, colour='blue'):
-            # desc(wim_path, category=['Index', 'Name', 'Description', 'Architecture', 'Languages', 'Installation'])
             wim_indexes += get_wiminfo(wim_path)
-        
-        for wim in wim_indexes: print(wim)
-        print()
-
         try:
-            # wim_indexes = sorted(wim_indexes, key=lambda x: (sort_criteria['Edition'].index(x.Edition), sort_criteria['Architecture'].index(x.Architecture)))
-            # wim_indexes = sorted(wim_indexes, key=lambda x: (sort_criteria['Edition'].index(x.Edition)))
-            # sort_keys = sort_criteria.keys()
             wim_indexes = sorted(wim_indexes, key=sort_wim)
-        except ValueError as e:
-            tqdm.write(f'{Fore.RED}{str(e)}{Style.RESET_ALL}')
         except Exception as e:
             tqdm.write(f'{Fore.RED}{str(e)}{Style.RESET_ALL}')
+            os.system('pause')
+            exit(0)
 
-        for wim in wim_indexes: print(wim)
+        # Summary sorted list
+        print('='*80)
+        for idx, wim in enumerate(wim_indexes):
+            print(f'{Fore.CYAN if idx % 2 else Fore.GREEN}[{idx+1}]\t{wim}{Style.RESET_ALL}')
+        print('='*80, '\nThe sorted results are as above.')
+        if enable_pause: os.system('pause')
+
+        # Export using DISM
+        for wim in tqdm(wim_indexes, desc='Exporting WIM', colour='green', leave=False, dynamic_ncols=True):
+            tqdm.write(f'{Fore.YELLOW}{wim}{Fore.WHITE} from {Fore.CYAN}{wim.Details_for_image}{Fore.WHITE}:{Fore.GREEN}{wim.Index}{Style.RESET_ALL}')
+            if purge_old_wim:
+                print(f'{Fore.YELLOW}Remove old wim: {Fore.RED}{dest_wim_fname}{Style.RESET_ALL}')
+                os.remove(dest_wim_fname)
+            export(wim.Details_for_image, dest_wim_fname, range(1, 2), check_integrity=False)
         
-        test_str = ['x64', 'x86']
-        test_str.sort(key=lambda e : (-ord(e[1])))
-
-        wim_indexes
-
-        import random
-        crit = ['Starter', 'Starter K', 'Starter KN', 'Starter N', 'Home', 'Professional', 'Education', 'Education N', 'Enterprise', 'Ultimate',]
-        test_str = ['Starter', 'Starter K', 'Starter KN', 'Starter N', 'Home', 'Professional', 'Education', 'Education N', 'Enterprise', 'Ultimate',]
-        random.shuffle(test_str)
-        try:
-            test_str.sort(key=lambda e : (crit.index(e)))
-        except AttributeError as e:
-            print()
-        except Exception as e:
-            print()
-
-        sorted_wims = sorted(wim_indexes, key=lambda e : (eval('e.' + 'Languages'), eval('e.Installation'), eval('e.Architecture'), eval('e.Name')))
-
-        for k, v in sort_criteria.items():
-            for wim in wim_indexes:
-                print(eval('wim.' + k))
-
-            
-        
-        for wim in wim_indexes:
-            print(wim.Name)
-
-    '''
-    Export wim-files to single wim file.
-    '''
-    if int(config['Features']['export_wim']):
-        criterias = ast.literal_eval(config['Export']['export_criteria'])
-        for criteria in criterias.keys():
-            for condition in criterias[criteria].split(','):
-                print(criteria, condition)
-
-    def sort_criterias(a):
-        print(a)
-        return 0
-
-    # print(sorted(['x64', 'x86', 'x64'], key=lambda x: x[2]))
-
-    # wim2swm('all.wim', 'install.swm', chk_integrity=True)
-
-    # for i in range(1, 50):
-        # desc("allinone.wim", idx=i)
-    # desc("allinone.wim", idx=1)
-    # desc("allinone.wim", category=['Index', 'Name', 'Description', 'Architecture', 'Languages', 'Installation'])
-    # desc("x86.wim", category=['Index', 'Name', 'Description', 'Architecture', 'Languages', 'Installation'])
-    # desc("x64.wim", category=['Index', 'Name', 'Description', 'Architecture', 'Languages', 'Installation'])
-    # desc("ko.wim", category=['Index', 'Name', 'Description', 'Architecture', 'Languages', 'Installation'])
-    
-    # for i in [38,44,39,45,40,46,41,47,42,48,43,49,50,51,52,53,54,55,56,57,58]:
-        # export('allinone.wim', i, 'ko.wim')
-    # for i in range(31, 38):
-        # export('allinone.wim', i, 'en.wim')
-    
-    # export('en.wim', i, 'en.wim')
-    # export('ko.wim', i, 'ko.wim')
-    # export('en.wim', 'all.wim')
-    # export('ko.wim', 'all.wim')
-
-    # desc("all.wim", category=['Index', 'Name', 'Description', 'Architecture', 'Languages', 'Installation'])
-    # desc("all.wim", category=['Name'])
-
-    # desc("en.wim", category=['Index', 'Name', 'Architecture'])
-    # desc("en.wim", category=[ 'Architecture'])
-
-    # for i in range(1, 59):
-    #     if i in [1,2,3,4,5,6,7,8,9,10,11,12,13,27,28,38,39,40,41,42,43,50]:
-    #         export('allinone.wim', i, 'x86.wim')
-    #     else:
-    #         export('allinone.wim', i, 'x64.wim')
-    
-    # os.system('dism /get-imageinfo /imagefile:allinone.wim')
-    # desc("C:\\Users\\Administrator\\Desktop\\install3-ko.wim")
-    # for i in range(1, 59):
-        # filts = desc("allinone.wim", i, ['Index', 'Name', 'Description', 'Architecture', 'Modified', 'Languages'])
-    
-    # for idx in range(1, 4):
-    #     export("C:\\Users\\Administrator\\Desktop\\home-x86.wim", idx, "C:\\Users\\Administrator\\Desktop\\install.wim")
-    # for idx in range(1, 11):
-    #     # if idx == 6: continue
-    #     export("N:\\sources\\install.wim", idx, "C:\\Users\\Administrator\\Desktop\\install.wim")
-    # for idx in range(1, 4):
-    #     export("C:\\Users\\Administrator\\Desktop\\home-x64.wim", idx, "C:\\Users\\Administrator\\Desktop\\install.wim")
-    # for idx in range(1, 11):
-    #     export("O:\\sources\\install.wim", idx, "C:\\Users\\Administrator\\Desktop\\install.wim")
-    # for idx in range(1, 12):
-    #     export("C:\\Users\\Administrator\\Desktop\\install3.wim", idx, "C:\\Users\\Administrator\\Desktop\\install.wim")
-    
-    
-    # export("P:\\sources\\install.wim", 1, "C:\\Users\\Administrator\\Desktop\\install.wim")
-    # for idx in range(1, 7):
-    #     export("R:\\sources\\install.wim", idx, "C:\\Users\\Administrator\\Desktop\\install.wim")
-    # export("Q:\\sources\\install.wim", 1, "C:\\Users\\Administrator\\Desktop\\install.wim")
-    # for idx in range(1, 7):
-    #     export("S:\\sources\\install.wim", idx, "C:\\Users\\Administrator\\Desktop\\install.wim")
-    
-    # for idx in range(1, 10):
-    #     export("C:\\Users\\Administrator\\Desktop\\install3-ko.wim", idx, "C:\\Users\\Administrator\\Desktop\\install.wim")
-    
-    # desc("C:\\Users\\Administrator\\Desktop\\install.wim")
-    # desc("E:\os\AllInOne\install.wim")
+        # Split WIM to SWM using DISM
+        if split_wim:
+            wim2swm(dest_wim_fname, dest_swm_fname, split_size, check_integrity=False)
--- wim.py
+++ wim.py
@@ -1,11 +1,13 @@
 class WIM:
     def __init__(self, output_dict) -> None:
-        self.__dict__.update(output_dict)
+        for k, v in output_dict.items():
+            setattr(self, k.replace(' ', '_'), v)
 
     def __repr__(self):
         return '{' + self.Name + ', ' + self.Architecture + ', ' + self.Languages + '}'
+    
+    def __str__(self) -> str:
+        return self.Name + ', ' + self.Architecture + ', ' + self.Languages
 
 if __name__ == '__main__':
-    args = ['fdas', 'fdfd']
-    wim = WIM(*args)
-    print(wim)
+    pass
